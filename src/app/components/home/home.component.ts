12a8eb194304cf65849d72646c79a4d6cd877f93
u1960297 <u1960297@campus.udg.edu>
2023-12-13 20:39:40 +0100

    Add time selector and apply filters to query

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,9 +4,8 @@ import { Recipes } from '../../models/recipes.model';
 import { Ingredients } from '../../models/ingredients.model';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
-import { getFirestore, doc, setDoc, updateDoc } from "firebase/firestore";
+import { getFirestore, doc, setDoc, updateDoc, where, DocumentData, Query, query } from "firebase/firestore";
 import { collection, getDoc, getDocs } from '@angular/fire/firestore';
-
 
 @Component({
   selector: 'app-home',
@@ -30,8 +29,14 @@ export class HomeComponent implements OnInit {
   filterName: string = '';
   filterDuration: number = 0;
   selectedDifficulty: string = '';
+  selectedTime: string = '';
   receipts: Recipes[] = [];
   filteredReceipts: Recipes[] = [];
+
+  times = [
+    {name: 'minuts', value: 'minuts'},
+    {name: 'hores', value: 'hores'}
+  ];
 
   async ngOnInit(): Promise<void> {
     this.receipts = await this.getReceptes(); // Crida a la firebase per obtenir receptes.
@@ -47,25 +52,34 @@ export class HomeComponent implements OnInit {
     this.router.navigate(['/login']);
   }
 
-  async getReceptes(): Promise<Recipes[]> { // Ara tenim receptes de prova
+  async getReceptes(): Promise<Recipes[]> {
     const recipesRef = collection(this.db, "recipes");
+    let firebaseQuery: Query<DocumentData> = recipesRef; // Canviem el nom de la variable a 'firebaseQuery'
+  
+    // Aplica els filtres a la consulta
+    if (this.filterName) {
+      firebaseQuery = query(firebaseQuery, where('name', '>=', this.filterName), where('name', '<=', this.filterName + '\uf8ff'));
+    }
+    if (this.filterDuration) {
+      console.log(this.filterDuration + " " + this.selectedTime);
+      firebaseQuery = query(firebaseQuery, where('time', '==', this.filterDuration + " " + this.selectedTime));
+    }
+    if (this.selectedDifficulty) {
+      firebaseQuery = query(firebaseQuery, where('difficulty', '==', this.selectedDifficulty));
+    }
+  
+    const querySnapshot = await getDocs(firebaseQuery);
     const receptes: Recipes[] = [];
-    const querySnapshot = await getDocs(recipesRef);
-
+  
     querySnapshot.forEach((doc) => {
       console.log(`${doc.id} => ${doc.data()}`);
       const recepta = { id: doc.id, ...doc.data() } as Recipes;
-      //Part de filtratge
-      if (recepta.name && recepta.name.toLowerCase().indexOf(this.filterName.toLowerCase()) !== -1) {
-        if (this.filterDuration === 0 || (recepta.time && (recepta.time.includes('minuts') && +parseInt(recepta.time)/60 <= this.filterDuration || recepta.time.includes('hores') && +parseInt(recepta.time) <= this.filterDuration))) {
-          if (this.selectedDifficulty === '' || recepta.difficulty === this.selectedDifficulty) {
-            receptes.push(recepta);
-          }
-        }
-      }
+      receptes.push(recepta);
     });
+  
     return receptes as Recipes[];
   }
+  
 
   goToProfile(): void { // Metode per anar al perfil.
     this.router.navigate(['/profile']);
